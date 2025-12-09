c4728fd79f13a3c087867f2539d3b1daddea4c0b
liangshan <liang.shan@zhaopin.com.cn>
2025-12-09 22:06:32 +0800

    no message

--- src/pages/playground/modals/ChatModal.vue
+++ src/pages/playground/modals/ChatModal.vue
@@ -1,15 +1,15 @@
 <template>
-  <view class="w-full h-[800rpx] flex flex-col justify-between" >
-    <view class="w-full h-full p-[32rpx] box-border" :style="{maxHeight: `calc(100% - 120rpx - 100rpx - 64rpx - ${safeBottom}px)`}">
+  <view class="relative w-full h-[800rpx] flex flex-col justify-between" >
+    <view class="w-full h-full p-[32rpx] box-border" :style="{maxHeight: `calc(100% - 120rpx - 100rpx - ${safeBottom}px)`}">
       <view class="w-full max-h-full py-[32rpx] box-border rounded-[32rpx] overflow-hidden bg-[#07c160] shadow-[0_8rpx_32rpx_rgba(0,0,0,0.15)] flex flex-row">
-        <view class="w-full h-full px-[32rpx] box-border overflow-auto">
+        <view class="w-full max-h-full px-[32rpx] box-border overflow-auto">
           <textarea type="text" class="text-[#fff]" auto-height/>
         </view>
       </view>
     </view>
 
-    <view class="w-full h-[120rpx] bg-red-500"></view>
-    <view class="w-full" :style="{ height: `calc(100rpx + ${safeBottom}px)` }"></view>
+    <view class="absolute left-0 w-full h-[120rpx] bg-red-500" :style="{bottom: `calc(100rpx + ${safeBottom}px)`}"></view>
+    <view class="absolute left-0 bottom-0 w-full" :style="{ height: `calc(100rpx + ${safeBottom}px)` }"></view>
   </view>
 </template>
 
